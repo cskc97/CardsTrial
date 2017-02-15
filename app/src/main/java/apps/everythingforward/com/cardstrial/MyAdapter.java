cf4fa6e67c0620b2e77db099e808fc5f50fe82a5
Santhoshkrishnachaitanya Chelikavada <everythingskc@gmail.com>
2017-02-15 14:11:02 +0800

    Working on Recycler View

---
+++ app/src/main/java/apps/everythingforward/com/cardstrial/MyAdapter.java
@@ -0,0 +1,39 @@
+package apps.everythingforward.com.cardstrial;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by santh on 2/15/2017.
+ */
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+
+
+    }
+
+
+    @Override
+    public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(MyAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+
+}
